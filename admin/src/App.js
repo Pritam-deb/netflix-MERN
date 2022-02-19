b9dbab610134239aa486b346cb70a46e5b04b133
Pritam Debnath <49619791+Pritam-deb@users.noreply.github.com>
2022-02-19 23:06:35 +0530

    feat: added a userlist page

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
+import UserList from "./pages/userList/UserList";
 import {
   BrowserRouter as Router,
   Routes,
@@ -12,16 +13,26 @@ import Home from "./pages/home/Home";
 function App() {
   return (
     // <Router>
+    //   <Routes>
+    //     <Navbar />
+    //     <div className="container">
+    //       <Sidebar />
+    //       <Route exact path="/">
+    //         <Home />
+    //       </Route>
+    //       <Route path="/users">
+    //         <UserList />
+    //       </Route>
+    //     </div>
+    //   </Routes>
+    // </Router>
     <div>
       <Navbar />
       <div className="container">
         <Sidebar />
-        {/* <Route exact path="/"> */}
-        <Home />
-        {/* </Route> */}
+        <UserList />
       </div>
     </div>
-    // </Router>
   );
 }
 
