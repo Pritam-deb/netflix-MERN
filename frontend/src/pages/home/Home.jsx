ffe4842e6ebbe4aa815c5431224b16adf441d889
Pritam Debnath <49619791+Pritam-deb@users.noreply.github.com>
2022-02-20 01:21:47 +0530

    feat: added movie slider with hover effect

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,12 +2,16 @@ import "./Home.scss";
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
+import List from "../../components/list/List";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <Featured />
+      <Featured type="movie" />
+      <List />
+      <List />
+      <List />
     </div>
   );
 };
