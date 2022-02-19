9235e8f9e8db530860e943f4647cd795a4c52b26
Pritam Debnath <49619791+Pritam-deb@users.noreply.github.com>
2022-02-20 02:46:22 +0530

    feat: added router according to user in FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
 import "./App.scss";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Register";
 import Login from "./pages/login/Login";
 function App() {
+  const user = true;
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={user ? <Home /> : <Register />} />
+        <Route
+          exact
+          path="/register"
+          element={!user ? <Register /> : <Home />}
+        />
+        <Route exact path="/Login" element={!user ? <Login /> : <Home />} />
         <Route exact path="/watch" element={<Watch />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/Login" element={<Login />} />
       </Routes>
     </Router>
   );
