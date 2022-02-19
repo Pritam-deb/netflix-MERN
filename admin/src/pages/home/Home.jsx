0b88333f8f01ead06f0b6ce54adf77ce40285301
Pritam Debnath <49619791+Pritam-deb@users.noreply.github.com>
2022-02-19 21:59:14 +0530

    feat: added small extra widgets

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import "./Home.css";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import Chart from "../../components/chart/Chart";
 import { userData } from "../../dummyData";
+import WidgetSmall from "../../components/widgetSmall/WidgetSmall";
 const Home = () => {
   return (
     <div className="home">
@@ -14,6 +15,9 @@ const Home = () => {
         grid
         dataKey="Active User"
       />
+      <div className="homeWidgets">
+        <WidgetSmall />
+      </div>
     </div>
   );
 };
