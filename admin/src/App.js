a171c9a74a79015d31c7b7746b485e16eade7db0
Pritam Debnath <49619791+Pritam-deb@users.noreply.github.com>
2022-02-19 21:21:05 +0530

    feat: added sidebar

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
+    // <div>
+    //   <Router>
+    //     <Routes>
+    //       <div className="container">
+    //       </div>
+    //     </Routes>
+    //   </Router>
+    // </div>
+    <>
       <Navbar />
-    </div>
+      <Sidebar />
+    </>
   );
 }
 
