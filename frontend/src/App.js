bf53e897ece1ef721fe0446df91db3e4b6c22f31
Pritam Debnath <49619791+Pritam-deb@users.noreply.github.com>
2022-02-20 01:30:04 +0530

    feat: added a page for movie player

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,13 @@ import {
   Redirect,
 } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch";
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/watch" element={<Watch />} />
       </Routes>
     </Router>
   );
