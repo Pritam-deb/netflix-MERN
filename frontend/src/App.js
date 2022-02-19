4c73d916a66c7b2836311c614712d8427d699ea8
Pritam Debnath <49619791+Pritam-deb@users.noreply.github.com>
2022-02-20 01:38:42 +0530

    feat: added login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import {
 import Home from "./pages/home/Home";
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 function App() {
   return (
     <Router>
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/watch" element={<Watch />} />
         <Route exact path="/register" element={<Register />} />
+        <Route exact path="/Login" element={<Login />} />
       </Routes>
     </Router>
   );
