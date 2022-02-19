3cd4ac5dca6c7b58082135c32a42cbb920edc1a3
Pritam Debnath <49619791+Pritam-deb@users.noreply.github.com>
2022-02-20 01:33:18 +0530

    feat: added registration page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,14 @@ import {
 } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/watch" element={<Watch />} />
+        <Route exact path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
